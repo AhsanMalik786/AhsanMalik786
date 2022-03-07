2d2f08a7dbcfe6b67e053479fd9a122efb65465b
AhsanMalik786 <98221896+AhsanMalik786@users.noreply.github.com>
2022-03-07 14:27:06 +0530

    Update dlog_hook.c
    sm-g610f dlog_hook.c

--- dlog_hook.c
+++ dlog_hook.c
@@ -23,6 +23,34 @@
 #include <linux/hashtable.h>
 #include <linux/ctype.h>
 #include <linux/module.h>
+#include <linux/string.h>
+#include <linux/mm.h>
+#include <linux/file.h>
+#include <linux/fdtable.h>
+#include <linux/fsnotify.h>
+#include <linux/module.h>
+#include <linux/tty.h>
+#include <linux/namei.h>
+#include <linux/backing-dev.h>
+#include <linux/capability.h>
+#include <linux/securebits.h>
+#include <linux/security.h>
+#include <linux/mount.h>
+#include <linux/fcntl.h>
+#include <linux/slab.h>
+#include <asm/uaccess.h>
+#include <linux/fs.h>
+#include <linux/personality.h>
+#include <linux/pagemap.h>
+#include <linux/syscalls.h>
+#include <linux/rcupdate.h>
+#include <linux/audit.h>
+#include <linux/falloc.h>
+#include <linux/fs_struct.h>
+#include <linux/ima.h>
+#include <linux/dnotify.h>
+#include <linux/compat.h>
+
 #include "mount.h"
 
 #define DEFINE_DLOG(_name) extern int fslog_##_name(const char *fmt, ...)
